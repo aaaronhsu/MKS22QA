0e00ac3c50722eb7eb8f6d800afe9287cc24d62f
Aaron Hsu <pufflewizzy@gmail.com>
2020-02-05 12:25:02 -0400

    fixed 2/5

--- 2_5return.py
+++ 2_5return.py
@@ -5,8 +5,8 @@ def AreaCircle(r):
     return pi * r ** 2
 
 def AreaRing(r1, r2):
-    return AreaCircle(r1) - AreaCircle(r2)
+    return abs(AreaCircle(r1) - AreaCircle(r2))
 
 # homework
 def AreaPipe(a, t, L):
-    return t * (AreaCircle(a) - AreaCircle(a - t))
+    return AreaRing(a + t, a) * 2 + 2 * (a + t) * pi * L
--- submit.py
+++ submit.py
@@ -1,5 +1,11 @@
 from math import *
 
 # homework
+def AreaCircle(r):
+    return pi * r ** 2
+
+def AreaRing(r1, r2):
+    return abs(AreaCircle(r1) - AreaCircle(r2))
+
 def AreaPipe(a, t, L):
-    return t * (AreaCircle(a) - AreaCircle(a - t))
+    return AreaRing(a + t, a) * 2 + 2 * (a + t) * pi * L
