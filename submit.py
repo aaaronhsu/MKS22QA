359313fad678d2ce74ad837b047cfddb348805c3
Aaron Hsu <pufflewizzy@gmail.com>
2020-02-07 14:12:34 -0400

    2/7

--- submit.py
+++ submit.py
@@ -1,18 +1,7 @@
-# homework
-def totalDuration(h1, m1, s1, h2, m2, s2):
-    s3 = (s1 + s2) % 60
-    m3 = (m1 + m2 + (int((s1 + s2) / 60))) % 60
-    h3 = (h1 + h2 + int((m1 + m2 + (int((s1 + s2) / 60))) / 60)) % 60
-    return str(h3) + ":" + str(m3) + ":" + str(s3)
+def fact(n):
+    if n < 2:
+        return 1
+    else:
+        print( n * fact(n - 1))
 
-# challenge
-def totalDurationChallenge(h1, m1, s1, h2, m2, s2):
-    un = totalDuration(h1, m1, s1, h2, m2, s2).split(":")
-    if int(un[0]) < 10:
-        un[0] = "0" + un[0]
-    if int(un[1]) < 10:
-        un[1] = "0" + un[1]
-    if int(un[2]) < 10:
-        un[2] = "0" + un[2]
-
-    return un[0] + ":" + un[1] + ":" + un[2]
+fact(5)
