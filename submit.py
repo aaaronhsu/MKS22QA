789c02ff7ea88cb6edd517ed4f3131589f69b43e
Aaron Hsu <pufflewizzy@gmail.com>
2020-02-24 12:03:26 -0400

    push

--- submit.py
+++ submit.py
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
 
 def getNthDigit(n, d):
     return int((n / (10 ** (d - 1))) % 10)
 
 print(getNthDigit(283107, 4))
-=======
-i = 0
-
-while i < 10:
-    print(i)
-    i += 1
-
-    if(i == 6):
-        break
-else:
-    print("ended")
->>>>>>> dbbf1e61626ec942359d4612595a6ee0d780362d
