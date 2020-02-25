e74defdac4769a62069042d282bd22229dd82081
Aaron Hsu <pufflewizzy@gmail.com>
2020-02-25 12:36:28 -0400

    2/25

--- 2_25moreStrings.py
+++ 2_25moreStrings.py
@@ -25,3 +25,28 @@ def index(s, c):
     for i in range(0, len(s)):
         if(s[i] == c):
             print("Current character", s[i], "position at", i)
+
+
+# challenge
+def replace(s):
+    n = s.find("not")
+    p = s.find("poor")
+    if n == -1 or p == -1:
+        return s
+
+    ans = ""
+    n += 3
+
+    return s[:p] + "good" + s[n:]
+
+def caesar(s, shift):
+    ans = ""
+    for i in range(0, len(s)):
+        if 97 <= ord(s[i]) <= 122:
+            ans += chr((((ord(s[i]) - 97) - shift) % 26) + 97)
+        elif 65 <= ord(s[i]) <= 90:
+            ans += chr((((ord(s[i]) - 65) - shift) % 26) + 65)
+        else:
+            ans += s[i]
+
+    return ans
