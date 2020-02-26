3b55439e425662455ff8ffe2be8dae6c3820e218
Aaron Hsu <pufflewizzy@gmail.com>
2020-02-26 16:05:52 -0400

    fixed hw 2

--- 2_26stringReview.py
+++ 2_26stringReview.py
@@ -21,17 +21,22 @@ def removeSpace(s):
 def capitalize(s):
     build = ""
     space = True
-    for i in range(len(s)):
+    for i in range(len(s) - 1):
         if space:
             build += s[i].upper()
             space = False
+        elif s[i + 1] == " ":
+            build += s[i].upper()
         elif s[i] == " ":
             build += " "
             space = True
         else:
             build += s[i]
 
+    build += s[len(s) - 1].upper()
     return build
+
+print(capitalize("hello this dog is fat"))
 
 # challenge
 def switch(s):
--- submit.py
+++ submit.py
@@ -1,15 +1,34 @@
-# classwork
-def toUpper(s):
-    return s.upper(), s.lower()
+# homework
+def removeSpace(s):
+    return s.replace(" ", "")
 
+def capitalize(s):
+    build = ""
+    space = True
+    for i in range(len(s) - 1):
+        if space:
+            build += s[i].upper()
+            space = False
+        elif s[i + 1] == " ":
+            build += s[i].upper()
+        elif s[i] == " ":
+            build += " "
+            space = True
+        else:
+            build += s[i]
 
-def testUpper(s):
-    numUpper = 0
-    for i in range(0, 4):
+    build += s[len(s) - 1].upper()
+    return build
+
+print(capitalize("hello this dog is fat"))
+
+# challenge
+def switch(s):
+    build = ""
+    for i in range(len(s)):
         if s[i].isupper():
-            numUpper += 1
+            build += s[i].lower()
+        else:
+            build += s[i].upper()
 
-    if numUpper >= 2:
-        return s.upper()
-    else:
-        return s
+    return build
