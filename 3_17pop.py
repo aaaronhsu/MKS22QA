491078c7a3d135ccbd60d1d949c8d3f53add3d13
Aaron Hsu <pufflewizzy@gmail.com>
2020-03-17 13:38:53 -0400

    pop

---
+++ 3_17pop.py
@@ -0,0 +1,19 @@
+# classwork
+def duplicates(l):
+    found = []
+
+    for i in range(len(l) - 1, -1, -1):
+        if l[i] in found:
+            l.pop(i)
+        else:
+            found += [l[i]]
+
+    return l
+
+# taking user input requires casting data to either an integer or string before removal, use parameter
+def delete(l, rem):
+    l.remove(rem)
+    return l
+
+
+print(delete([1, 2, 3, 4]))
