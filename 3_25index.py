44a2631aa5ff55f909727a0b676607d50388987b
Aaron Hsu <pufflewizzy@gmail.com>
2020-03-25 08:32:10 -0400

    3/25

---
+++ 3_25index.py
@@ -0,0 +1,70 @@
+# classwork
+# append
+a = ["hello"]
+a.append("sir")
+print(a)
+
+# clear
+l = ["This", "list", "contains", "things"]
+a.clear()
+print(a)
+
+# copy
+l = ["potato"]
+a = l.copy()
+print(a)
+
+# count
+l = [1, 2, 3, 4, 5, 2, 3, 2]
+a = l.count(2)
+print(a)
+
+# extend
+l = ["I"]
+l.extend(["want", "to", "sleep"])
+print(l)
+
+# max
+l = [1, 4, 2, 6, 2, 6, 8, 3]
+max = max(l)
+print(max)
+
+# min
+l = [1, 4, 2, 6, 2, 6, 8, 3]
+min = min(l)
+print(min)
+
+# index
+l = [1, 5, 2, 3]
+index = l.index(5)
+print(index)
+
+# insert
+l = [1, 2, 4, 5]
+l.insert(2, 3)
+print(l)
+
+# pop
+l = [4, 3, 2, 2, 1]
+l.pop(2)
+print(l)
+
+# remove
+l = [1, 2, 3, 3, 4, 5]
+l.remove(3)
+print(l)
+
+# reverse
+l = [1, 2, 3, 4]
+l.reverse()
+print(l)
+
+# sort
+l = [2, 3, 1, 4]
+l.sort()
+print(l)
+
+# sum
+l = [1, 2, 3, 4]
+a = sum(l)
+print(a)
--- _submit.py
+++ _submit.py
@@ -1,5 +1,21 @@
+import random
 
-l = [[1, 2, 3, 4], [2, 1, 3, 4], [4, 2, 3, 1], [2, 3, 4, 1]]
+# homework
+def buildRem():
+    num = int(input("Enter the number of elements in list: "))
+    nums = []
+    for i in range(num):
+        nums += [input("Enter element " + str(i + 1) + ": ")]
+    print("Current list is " + str(nums))
+    nums.remove(input("Enter word to remove: "))
+    return nums
 
-for i in l:
-    print(i)
+# challenge
+def password(length):
+    chars = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*\'"
+    pas = ""
+
+    for i in range(length):
+        pas += chars[random.randint(0, 46)]
+
+    return pas
